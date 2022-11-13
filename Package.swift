560442630c8a9d91fd1153890b8f14b66b8ba311
Max Obermeier <themomax@icloud.com>
2022-11-13 08:35:48 +0100

    add swift docc plugin

--- Package.swift
+++ Package.swift
@@ -14,7 +14,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-docc-symbolkit", branch: "main")
+        .package(url: "https://github.com/apple/swift-docc-symbolkit", branch: "main"),
+        .package(url: "https://github.com/themomax/swift-docc-plugin", branch: "add-extended-types-flag"),
+        // .package(path: "../../apple/swift-docc-plugin"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
